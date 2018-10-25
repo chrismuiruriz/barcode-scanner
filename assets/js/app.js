e4c6df0b594b6be8b527c0bf29357652b5108c6b
codemoranKE <codemorann@gmail.com>
2018-10-25 16:40:29 +0300

    Changed server

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
 /* Dom7 */
 var $$ = Dom7;
-$mainUrl = "http://localhost/smollan-api/api/";
+$mainUrl = "http://dev.bean.co.ke/smollan-api/api/";
 /* Theme */
 var theme = 'auto';
 if (document.location.search.indexOf('theme=') >= 0) {
